c35742a15061163471fef5806595489c943b71ca
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-03-24 12:24:17 +0530

    Updates

---
+++ precache-manifest.67e35641253a65edcce6c01ecb2b41dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac24b0f0f2712557a5d29a84205d449b",
+    "url": "/Covid-19-sri-lanka-statistics/index.html"
+  },
+  {
+    "revision": "ddce4525c840d7de0802",
+    "url": "/Covid-19-sri-lanka-statistics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "baf64381acea0631edf2",
+    "url": "/Covid-19-sri-lanka-statistics/static/js/2.46d073c1.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/Covid-19-sri-lanka-statistics/static/js/2.46d073c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddce4525c840d7de0802",
+    "url": "/Covid-19-sri-lanka-statistics/static/js/main.c8a834c3.chunk.js"
+  },
+  {
+    "revision": "6d1c116e31123f66fdb7",
+    "url": "/Covid-19-sri-lanka-statistics/static/js/runtime-main.bb75f9d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid-19-sri-lanka-statistics/static/media/logo.5d5d9eef.svg"
+  }
+]);
